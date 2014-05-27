b672ffc0655d981ac8a0db7b47002220f338bf5f
Martin Aarhof <martin.aarhof@gmail.com>
2014-05-27 17:35:24 +0200

    new design

--- src/Ksp/ScienceBundle/Resources/public/js/science.js
+++ src/Ksp/ScienceBundle/Resources/public/js/science.js
@@ -19,3 +19,42 @@ jQuery(function($) {
     });
     
 });
+
+function UpdateTableHeaders() {
+   $(".panel").each(function() {
+       var el             = $(this),
+           offset         = el.offset(),
+           scrollTop      = $(window).scrollTop() + 50,
+           floatingHeader = $(".floatingHeader", this)
+       
+       if ((scrollTop > offset.top) && (scrollTop < offset.top + (el.height() - 50))) {
+           floatingHeader.css({
+            "visibility": "visible"
+           });
+       } else {
+           floatingHeader.css({
+            "visibility": "hidden"
+           });      
+       };
+   });
+}
+
+// DOM Ready      
+$(function() {
+
+   var clonedHeaderRow;
+
+   $(".panel").each(function() {
+       clonedHeaderRow = $(".panel-heading", this);
+       clonedHeaderRow
+         .before(clonedHeaderRow.clone())
+         .css("width", clonedHeaderRow.outerWidth())
+         .addClass("floatingHeader");
+         
+   });
+   
+   $(window)
+    .scroll(UpdateTableHeaders)
+    .trigger("scroll");
+   
+});
