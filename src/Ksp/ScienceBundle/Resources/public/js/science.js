5ea36f88da22cdea4013a39cb74f13ef3b1e3a1b
Martin Aarhof <martin.aarhof@gmail.com>
2014-05-28 16:57:10 +0200

    scroll top - fixes #5

--- src/Ksp/ScienceBundle/Resources/public/js/science.js
+++ src/Ksp/ScienceBundle/Resources/public/js/science.js
@@ -1,6 +1,21 @@
-jQuery(function($) {
-    
-    $('.panel-heading').click(function() {
+jQuery(function ($) {
+
+    var clonedHeaderRow;
+
+    $(".panel").each(function () {
+        clonedHeaderRow = $(".panel-heading", this);
+        clonedHeaderRow
+            .before(clonedHeaderRow.clone())
+            .css("width", clonedHeaderRow.outerWidth())
+            .addClass("floatingHeader");
+
+    });
+
+    $(window)
+        .scroll(UpdateTableHeaders)
+        .trigger("scroll");
+
+    $('.panel-heading').click(function () {
         var body = $(this).next();
         if (body.css('display') == 'none') {
             body.show();
@@ -8,8 +23,8 @@ jQuery(function($) {
             body.hide();
         }
     });
-    
-    $('.place-heading').click(function() {
+
+    $('.place-heading').click(function () {
         var body = $(this).next();
         if (body.css('display') == 'none') {
             body.show();
@@ -17,44 +32,33 @@ jQuery(function($) {
             body.hide();
         }
     });
-    
+
+    $(".nav a[href]").click(function () {
+        var target = $(this).attr("href");
+        target = target.substring(1, target.length);
+        $('html, body').animate({
+            scrollTop: $('[id="' + target + '"]').offset().top - 100
+        }, 500);
+        return false;
+    });
+
 });
 
 function UpdateTableHeaders() {
-   $(".panel").each(function() {
-       var el             = $(this),
-           offset         = el.offset(),
-           scrollTop      = $(window).scrollTop() + 50,
-           floatingHeader = $(".floatingHeader", this)
-       
-       if ((scrollTop > offset.top) && (scrollTop < offset.top + (el.height() - 50))) {
-           floatingHeader.css({
-            "visibility": "visible"
-           });
-       } else {
-           floatingHeader.css({
-            "visibility": "hidden"
-           });      
-       };
-   });
+    $(".panel").each(function () {
+        var el = $(this),
+            offset = el.offset(),
+            scrollTop = $(window).scrollTop() + 50,
+            floatingHeader = $(".floatingHeader", this);
+
+        if ((scrollTop > offset.top) && (scrollTop < offset.top + (el.height() - 50))) {
+            floatingHeader.css({
+                "visibility": "visible"
+            });
+        } else {
+            floatingHeader.css({
+                "visibility": "hidden"
+            });
+        }
+    });
 }
-
-// DOM Ready      
-$(function() {
-
-   var clonedHeaderRow;
-
-   $(".panel").each(function() {
-       clonedHeaderRow = $(".panel-heading", this);
-       clonedHeaderRow
-         .before(clonedHeaderRow.clone())
-         .css("width", clonedHeaderRow.outerWidth())
-         .addClass("floatingHeader");
-         
-   });
-   
-   $(window)
-    .scroll(UpdateTableHeaders)
-    .trigger("scroll");
-   
-});
